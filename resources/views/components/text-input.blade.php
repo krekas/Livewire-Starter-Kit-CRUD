60d9f4f6215d867db754da849d92e41292148a1a
krekas <krekas@gmail.com>
2025-04-08 09:47:07 +0300

    Show validation in red

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -6,5 +6,7 @@
 <fieldset class="fieldset">
     <legend class="fieldset-legend">{{ $label }}</legend>
     <input {{ $attributes->merge(['class' => 'input']) }} placeholder="{{ $placeholder }}" />
-    <p class="fieldset-label">You can edit page title later on from settings</p>
+    @error($attributes->first('wire:model'))
+        <p class="text-sm text-red-600 dark:text-red-400 space-y-1">{{ $message }}</p>
+    @enderror
 </fieldset>
